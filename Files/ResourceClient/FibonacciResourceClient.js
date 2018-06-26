aa262773b10628c0fa295eda3bc5233d0091c762
katirox9 <katirox9@gmail.com>
2018-06-25 21:48:00 -0600

    fibonacci

---
+++ Files/ResourceClient/FibonacciResourceClient.js
@@ -0,0 +1,20 @@
+class FibonacciResourceClient{
+    constructor(){
+        this._urlfibonacci = "";
+    }
+    get url(){
+        return this._urlfibonacci;
+    }
+    findBySuccession(array){
+        let init={
+            method: 'POST',
+            headers: {
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(array)
+        };
+        return fetch(this._urlfibonacci,init);
+    }
+    
+}
+export default FibonacciResourceClient;
